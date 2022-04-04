ed332b6980082c975f74301c43fb29a4c8594295
jakidxav <joshuadriscoll@it-d4jctsc1.colleges.ad.unm.edu>
2022-04-04 15:54:53 -0600

    adding background color for app
    using bootstrap navbar to separate out pages/different maps
    
    incorporating easy print leaflet plugin so that now users can download png of maps

---
+++ src/map_slider.js
@@ -0,0 +1,40 @@
+"use strict"; // JS strict mode
+
+window.onload = function () {
+    // create map container, add basemap
+    var map = L.map("map_slider_container", {
+        center: [39.73, -104.99], //[35.08770657898809, -106.65591268675824]
+        zoom: 11,
+    }).setView([35.08770657898809, -106.65591268675824], 11);
+
+    // define basemap here
+    var basemapAttribution =
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
+    var basemapURL =
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
+
+    var streets = L.tileLayer(basemapURL, {
+        id: "mapbox/streets-v11",
+        attribution: basemapAttribution,
+        tileSize: 512,
+        zoomOffset: -1,
+        maxZoom: 18,
+    }).addTo(map);
+
+    var printer = L.easyPrint({
+        tileLayer: streets,
+        sizeModes: ['Current', 'A4Landscape', 'A4Portrait'],
+        filename: 'abq_map_slider',
+        exportOnly: true,
+        hideControlContainer: true
+    }).addTo(map);
+
+    /*
+    console.log("map loaded")
+    console.log(map.getBounds());
+    map.on('moveend', function () {
+        console.log(map.getBounds());
+    });
+    */
+};
